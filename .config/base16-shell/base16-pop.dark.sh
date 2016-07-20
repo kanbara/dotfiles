60ac230768006dd073400b26bf240afb232ea591
Jay Morrow <jay@planet.com>
2016-07-20 16:42:33 +0200

    fixed pop shell and added random bg changing

--- .config/base16-shell/base16-pop.dark.sh
+++ .config/base16-shell/base16-pop.dark.sh
@@ -2,59 +2,100 @@
 # Base16 Pop - Console color setup script
 # Chris Kempson (http://chriskempson.com)
 
-color00="00/00/00" # Base 00 - Black
-color01="eb/00/8a" # Base 08 - Red
-color02="37/b3/49" # Base 0B - Green
-color03="f8/ca/12" # Base 0A - Yellow
-color04="0e/5a/94" # Base 0D - Blue
-color05="b3/1e/8d" # Base 0E - Magenta
-color06="00/aa/bb" # Base 0C - Cyan
-color07="d0/d0/d0" # Base 05 - White
-color08="50/50/50" # Base 03 - Bright Black
-color09=$color01 # Base 08 - Bright Red
-color10=$color02 # Base 0B - Bright Green
-color11=$color03 # Base 0A - Bright Yellow
-color12=$color04 # Base 0D - Bright Blue
-color13=$color05 # Base 0E - Bright Magenta
-color14=$color06 # Base 0C - Bright Cyan
-color15="ff/ff/ff" # Base 07 - Bright White
-color16="f2/93/33" # Base 09
-color17="7a/2d/00" # Base 0F
-color18="20/20/20" # Base 01
-color19="30/30/30" # Base 02
-color20="b0/b0/b0" # Base 04
-color21="e0/e0/e0" # Base 06
+if [ "${TERM%%-*}" = 'linux' ]; then
+    # This script doesn't support linux console (use 'vconsole' template instead)
+    return 2>/dev/null || exit 0
+fi
+
+
+color00="00/00/00"
+color01="eb/00/8a"
+color02="37/b3/49"
+color03="f8/ca/12"
+color04="0e/5a/94"
+color05="b3/1e/8d"
+color06="00/aa/bb"
+color07="d0/d0/d0"
+color08="50/50/50"
+color09="eb/00/8a"
+color10="37/b3/49"
+color11="f8/ca/12"
+color12="0e/5a/94"
+color13="b3/1e/8d"
+color14="00/aa/bb"
+color15="ff/ff/ff"
+color16="f2/93/33"
+color17="7a/2d/00"
+color18="20/20/20"
+color19="30/30/30"
+color20="b0/b0/b0"
+color21="e0/e0/e0"
+color_foreground="d0/d0/d0"
+color_background="00/00/00"
+color_cursor="d0/d0/d0"
+
+if [ -n "$TMUX" ]; then
+  # tell tmux to pass the escape sequences through
+  # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+  printf_template="\033Ptmux;\033\033]4;%d;rgb:%s\007\033\\"
+  printf_template_var="\033Ptmux;\033\033]%d;rgb:%s\007\033\\"
+  printf_template_custom="\033Ptmux;\033\033]%s%s\007\033\\"
+elif [ "${TERM%%-*}" = "screen" ]; then
+  # GNU screen (screen, screen-256color, screen-256color-bce)
+  printf_template="\033P\033]4;%d;rgb:%s\007\033\\"
+  printf_template_var="\033P\033]%d;rgb:%s\007\033\\"
+  printf_template_custom="\033P\033]%s%s\007\033\\"
+else
+  printf_template="\033]4;%d;rgb:%s\033\\"
+  printf_template_var="\033]%d;rgb:%s\033\\"
+  printf_template_custom="\033]%s%s\033\\"
+fi
 
 # 16 color space
-printf "\e]4;0;rgb:$color00\e\\"
-printf "\e]4;1;rgb:$color01\e\\"
-printf "\e]4;2;rgb:$color02\e\\"
-printf "\e]4;3;rgb:$color03\e\\"
-printf "\e]4;4;rgb:$color04\e\\"
-printf "\e]4;5;rgb:$color05\e\\"
-printf "\e]4;6;rgb:$color06\e\\"
-printf "\e]4;7;rgb:$color07\e\\"
-printf "\e]4;8;rgb:$color08\e\\"
-printf "\e]4;9;rgb:$color09\e\\"
-printf "\e]4;10;rgb:$color10\e\\"
-printf "\e]4;11;rgb:$color11\e\\"
-printf "\e]4;12;rgb:$color12\e\\"
-printf "\e]4;13;rgb:$color13\e\\"
-printf "\e]4;14;rgb:$color14\e\\"
-printf "\e]4;15;rgb:$color15\e\\"
-
+printf $printf_template 0  $color00
+printf $printf_template 1  $color01
+printf $printf_template 2  $color02
+printf $printf_template 3  $color03
+printf $printf_template 4  $color04
+printf $printf_template 5  $color05
+printf $printf_template 6  $color06
+printf $printf_template 7  $color07
+printf $printf_template 8  $color08
+printf $printf_template 9  $color09
+printf $printf_template 10 $color10
+printf $printf_template 11 $color11
+printf $printf_template 12 $color12
+printf $printf_template 13 $color13
+printf $printf_template 14 $color14
+printf $printf_template 15 $color15
 
 # 256 color space
-if [ "$TERM" != linux ]; then
-  printf "\e]4;16;rgb:$color16\e\\"
-  printf "\e]4;17;rgb:$color17\e\\"
-  printf "\e]4;18;rgb:$color18\e\\"
-  printf "\e]4;19;rgb:$color19\e\\"
-  printf "\e]4;20;rgb:$color20\e\\"
-  printf "\e]4;21;rgb:$color21\e\\"
+printf $printf_template 16 $color16
+printf $printf_template 17 $color17
+printf $printf_template 18 $color18
+printf $printf_template 19 $color19
+printf $printf_template 20 $color20
+printf $printf_template 21 $color21
+
+# foreground / background / cursor color
+if [ -n "$ITERM_SESSION_ID" ]; then
+  # iTerm2 proprietary escape codes
+  printf $printf_template_custom Pg a6a28c # forground
+  printf $printf_template_custom Ph 20201d # background
+  printf $printf_template_custom Pi a6a28c # bold color
+  printf $printf_template_custom Pj 6e6b5e # selection color
+  printf $printf_template_custom Pk a6a28c # selected text color
+  printf $printf_template_custom Pl a6a28c # cursor
+  printf $printf_template_custom Pm 20201d # cursor text
+else
+  printf $printf_template_var 10 $color_foreground
+  printf $printf_template_var 11 $color_background
+  printf $printf_template_var 12 $color_cursor
 fi
 
 # clean up
+unset printf_template
+unset printf_template_var
 unset color00
 unset color01
 unset color02
@@ -77,3 +118,6 @@ unset color18
 unset color19
 unset color20
 unset color21
+unset color_foreground
+unset color_background
+unset color_cursor
--- scripts/rand.sh
+++ scripts/rand.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 ~/bin/themer activate $(~/bin/themer list | shuf -n 1)
+feh  --bg-fill $(find ~/Pictures/ps -type f | shuf -n 1) '/home/takuya/Pictures/bjango-trio-hot-vert.png'
+
